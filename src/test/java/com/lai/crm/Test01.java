c6bb9a8970a88373d7c5b584f8ae23505132e036
52zongzhen <2787747@qq.com>
2021-02-05 22:54:35 +0800

    20210205

--- src/test/java/com/lai/crm/Test01.java
+++ src/test/java/com/lai/crm/Test01.java
@@ -5,5 +5,29 @@ package com.lai.crm;/*
  */
 
 public class Test01 {
-    String s = "ww";
+    public static void main(String[] args) {
+        /*//验证失效时间
+        String exprireTime = "2021-02-05 22:42:55";
+        String da = DateTimeUtil.getSysTime();
+        int count = exprireTime.compareTo(da);
+        System.out.println(count);*/
+
+        /*//equals判断要把常量放前面
+        //判断是否锁定
+        String lockstate = "0";
+        if ("0".equals(lockstate)){
+            System.out.println("用户被锁定");
+        }*/
+
+        //断送IP地址
+        String ip = "192.168.8.90";
+        String allowIps = "192.168.8.91,127.0.0.1";
+        if(allowIps.contains(ip)){
+            System.out.println("IP地址有效");
+        }else {
+            System.out.println("IP地址无效");
+        }
+    }
+
+
 }
